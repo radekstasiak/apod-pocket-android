3e3a3b217658f107e94f1ce5293bdfb2595aff7e
radekstasiak <radek.stasiak@gmail.com>
2015-09-28 21:50:06 +0100

    Add apods list animation

--- app/src/main/java/com/example/radek/apodpocket/adapter/APODAdapter.java
+++ app/src/main/java/com/example/radek/apodpocket/adapter/APODAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.NetworkImageView;
@@ -15,6 +17,7 @@ import com.example.radek.apodpocket.model.APOD;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Random;
 
 /**
  * Created by Radek on 27/09/15.
@@ -68,6 +71,16 @@ public class APODAdapter extends RecyclerView.Adapter<APODAdapter.ViewHolder> {
             viewHolder.mTitle.setText(mDataset.get(position).getTitle());
             viewHolder.mDate.setText(mDataset.get(position).getDate());
             viewHolder.mElementImage.setImageUrl(mDataset.get(position).getUrl(), ImageCacheManager.getInstance().getImageLoader());
+
+            Animation mFadeAnimation  = AnimationUtils.loadAnimation(mContext,R.anim.blink);
+
+            int intMin = (int) (long) mFadeAnimation.getDuration() - 200;
+            int intMax = (int) (long) mFadeAnimation.getDuration() + 200;
+
+            int randNum = intMin + (int)(Math.random() * ((intMax - intMin) + 1));
+            mFadeAnimation.setDuration(randNum);
+
+            viewHolder.mElementImage.startAnimation(mFadeAnimation);
         }
     }
 
